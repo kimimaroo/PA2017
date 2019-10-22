f438cbf253fd95df2c37b74283c17afab2b94809
tracer-pa2017 <tracer@njuics.org>
2019-10-22 12:56:13 +0800

    >  nemu MF1933037 hxj Linux debian9 4.9.0-11-686-pae #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) i686 GNU/Linux  12:56:13 up  3:01,  2 users,  load average: 0.03, 0.03, 0.00 ef65925b9c21415774effc9160bd5aea801e53e8

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -79,8 +79,10 @@ uint32_t alu_add(uint32_t src, uint32_t dest) {
 uint32_t alu_adc(uint32_t src, uint32_t dest) {
 	int dest_sign = sign_flag_set(dest);
 	int src_sign = sign_flag_set(src);
-	// uint64_t dest_64 = src + dest + cpu.eflags.CF;
+	uint64_t dest_64 = src + dest + cpu.eflags.CF;
+	printf(dest_64\n);
 	dest = src + dest + cpu.eflags.CF;
+	printf(dest\n);
 	cpu.eflags.CF = 1;
 	cpu.eflags.PF = parity_flag_set(dest);
 	if(dest == 0){
