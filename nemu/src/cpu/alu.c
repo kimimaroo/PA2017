e94ab26a8c8a42f285f50318c803d03ae80b4008
tracer-pa2017 <tracer@njuics.org>
2019-10-23 21:18:12 +0800

    >  nemu MF1933037 hxj Linux debian9 4.9.0-11-686-pae #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) i686 GNU/Linux  21:18:12 up  2:26,  2 users,  load average: 0.08, 0.06, 0.04 e7679d3cf576ee8388ea626441ae6dcbbab87e5f

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -172,7 +172,7 @@ uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size) {
     cpu.eflags.CF = (src >= data_size) ? 0: (dest >> (data_size -src)) & 0x1;
     cpu.eflags.ZF = result == 0;
     cpu.eflags.SF = result >> (data_size - 1);
-    // cpu.eflags.PF = check_PF(result);
+    cpu.eflags.PF = parity_flag_set(result);
     if (src == 1) cpu.eflags.OF = dest >> (data_size - 1);
 
 	return result;
