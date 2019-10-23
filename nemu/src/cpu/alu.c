17236245e9876d07e4173ef675c8bd32a233cfb8
tracer-pa2017 <tracer@njuics.org>
2019-10-23 20:27:55 +0800

    >  nemu MF1933037 hxj Linux debian9 4.9.0-11-686-pae #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) i686 GNU/Linux  20:27:55 up  1:35,  2 users,  load average: 0.00, 0.02, 0.01 d7f33582f9773f7cb5ec73fab81b2c3b7f30a56

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -140,20 +140,27 @@ uint32_t alu_and(uint32_t src, uint32_t dest) {
 	cpu.eflags.PF = parity_flag_set(dest);
 	cpu.eflags.ZF = (dest == 0)? 1 : 0;
 	cpu.eflags.SF = sign_flag_set(dest);
-
 	return dest;
 }
 
 uint32_t alu_xor(uint32_t src, uint32_t dest) {
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
+	dest = dest ^ src;
+	cpu.eflags.CF = 0;
+	cpu.eflags.OF = 0;
+	cpu.eflags.PF = parity_flag_set(dest);
+	cpu.eflags.ZF = (dest == 0)? 1 : 0;
+	cpu.eflags.SF = sign_flag_set(dest);
+	return dest;
 }
 
 uint32_t alu_or(uint32_t src, uint32_t dest) {
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
+	dest = dest | src;
+	cpu.eflags.CF = 0;
+	cpu.eflags.OF = 0;
+	cpu.eflags.PF = parity_flag_set(dest);
+	cpu.eflags.ZF = (dest == 0)? 1 : 0;
+	cpu.eflags.SF = sign_flag_set(dest);
+	return dest;
 }
 
 uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size) {
