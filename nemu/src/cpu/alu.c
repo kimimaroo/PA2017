e9820b6b9457a53cd9173e920b81d23f1dedb760
tracer-pa2017 <tracer@njuics.org>
2019-10-24 18:59:45 +0800

    >  nemu MF1933037 hxj Linux debian9 4.9.0-11-686-pae #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) i686 GNU/Linux  18:59:45 up  4:47,  2 users,  load average: 0.00, 0.00, 0.00 a62f060ae7c23cfee78996bccd0ae6447ffd7a6c

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -104,9 +104,20 @@ uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size) {
 }
 
 int64_t alu_imul(int32_t src, int32_t dest, size_t data_size) {
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
+	int64_t result = 0;
+	int8_t src_8 = src & 0xff;
+	int8_t dest_8 = dest & 0xff;
+	int16_t src_16 = src & 0xffff;
+	int16_t dest_16 = dest & 0xffff;
+	switch(data_size){
+		case 8:
+			result = src_8 * dest_8;
+		case 16:
+			result = src_16 * dest_16;
+		case 32:
+			result = src * dest;
+	}
+	return result;
 }
 
 uint32_t alu_div(uint64_t src, uint64_t dest, size_t data_size) {
