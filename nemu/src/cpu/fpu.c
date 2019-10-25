ca570daf86071de9bc6860394b3fe6c96f10ad9d
tracer-pa2017 <tracer@njuics.org>
2019-10-25 14:55:19 +0800

    >  nemu MF1933037 hxj Linux debian9 4.9.0-11-686-pae #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) i686 GNU/Linux  14:55:19 up  4:52,  2 users,  load average: 0.13, 0.08, 0.02 293e834bab64c22e5edb3b96d6e6e80dad28d38f

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -114,6 +114,7 @@ uint32_t internal_float_add(uint32_t b, uint32_t a) {
 	}
 	if(a == P_ZERO_F || a == N_ZERO_F) { return b; }
 	if(b == P_ZERO_F || b == N_ZERO_F) { return a; }
+	printf("P_ZERO_F%x\n", P_ZERO_F);
 
 	printf("a%x\tb%x\n", a,b);
 	FLOAT f, fa, fb;
