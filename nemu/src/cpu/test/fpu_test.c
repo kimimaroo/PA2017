8c9fe74a705c4dea138fc36daa7d21305a353fd3
tracer-pa2017 <tracer@njuics.org>
2019-10-25 19:26:09 +0800

    >  nemu MF1933037 hxj Linux debian9 4.9.0-11-686-pae #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) i686 GNU/Linux  19:26:09 up  9:23,  2 users,  load average: 0.12, 0.04, 0.01 86e842ef551dd513b2a27bc3337cec24c88ea4fb

--- nemu/src/cpu/test/fpu_test.c
+++ nemu/src/cpu/test/fpu_test.c
@@ -33,6 +33,8 @@ void fpu_test_add() {
 		if(a.exponent == 0xff || b.exponent == 0xff) continue;
 		res.fval = a.fval + b.fval;
 		res_fpu.val = internal_float_add(b.val, a.val);
+
+			printf("float add a = %f, b = %f, ua = %x, ub = %x, res = %x, res_fpu = %x, res = %f, res_fpu = %f\n", a.fval, b.fval, a.val, b.val, res.val, res_fpu.val, res.fval, res_fpu.fval);
 		assert(res_fpu.val == res.val);
 	}
 	printf("fpu_test_add()  \e[0;32mpass\e[0m\n");
