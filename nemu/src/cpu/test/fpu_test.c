b6a2d98384292281d89a65e6d50c32544c703300
tracer-pa2017 <tracer@njuics.org>
2019-10-25 15:38:29 +0800

    >  nemu MF1933037 hxj Linux debian9 4.9.0-11-686-pae #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) i686 GNU/Linux  15:38:29 up  5:35,  2 users,  load average: 0.19, 0.15, 0.07 94391d6ad2b6d1622bd0024a002717cb3a36a4

--- nemu/src/cpu/test/fpu_test.c
+++ nemu/src/cpu/test/fpu_test.c
@@ -17,7 +17,7 @@ void fpu_test_add() {
 		for(j = 0 ; j < sizeof(input)/sizeof(float) ; j++) {
 			// printf(" == %d ==\n", count++);
 			a.fval = input[i];
-			printf("exp%x\tfrac%x\n",a.exponent,a.fraction);
+			printf("exp%x\tfrac%x\ta%f\n",a.exponent,a.fraction,a.fval);
 			b.fval = input[j];
 			res.fval = a.fval + b.fval;
 			res_fpu.val = internal_float_add(b.val, a.val);
