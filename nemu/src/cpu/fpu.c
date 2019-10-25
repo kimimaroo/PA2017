3ad8eee1bc5e29ba42250b162f2b7c5cbd5d02d9
tracer-pa2017 <tracer@njuics.org>
2019-10-25 19:38:22 +0800

    >  nemu MF1933037 hxj Linux debian9 4.9.0-11-686-pae #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) i686 GNU/Linux  19:38:22 up  9:35,  2 users,  load average: 0.03, 0.06, 0.01 e3db20967d395b068f068e998d9f4c2c2af634e

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -121,15 +121,7 @@ uint32_t internal_float_add(uint32_t b, uint32_t a) {
 	// infity, NaN
 	if(fa.exponent == 0xff) { return a; }
 	if(fb.exponent == 0xff) { return b; }
-	// if(fa.exponent == 0x0 && fa.fraction == 0x0) { return b; }
-	// if(fb.exponent == 0x0 && fb.fraction == 0x0) { return a; }
 	
-	// uint8_t fa_exponent, fb_exponent;
-	// fa_exponent = (fa.exponent == 0x0) ? fa.exponent + 1 : fa.exponent;
-	// fb_exponent = (fb.exponent == 0x0) ? fb.exponent + 1 : fb.exponent;
-	// if(fa.exponent == 0x0) { fa.exponent += 1; }
-	// if(fb.exponent == 0x0) { fb.exponent += 1; }
-
 	// 使得默认fa右移
 	if(fa.exponent > fb.exponent) {
 		fa.val = b;
@@ -146,8 +138,7 @@ uint32_t internal_float_add(uint32_t b, uint32_t a) {
 	uint32_t shift = 0;
 	
 	/* TODO: shift = ? */
-	shift = (fb.exponent == 0 ? fb.exponent + 1 : fb.exponent) - (fa.exponent == 0 ? fa.exponent + 1 : 
-fa.exponent);
+	shift = (fb.exponent == 0 ? fb.exponent + 1 : fb.exponent) - (fa.exponent == 0 ? fa.exponent + 1 : fa.exponent);
 	assert(shift >= 0);
 
 	sig_a = (sig_a << 3); // guard, round, sticky
