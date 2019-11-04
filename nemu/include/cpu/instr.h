4236a84c403744bfed538f6e1a9a17171e9ce590
tracer-pa2017 <tracer@njuics.org>
2019-11-04 22:52:08 +0800

    >  nemu MF1933037 hxj Linux debian9 4.9.0-11-686-pae #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) i686 GNU/Linux  22:52:08 up 1 day,  7:12,  2 users,  load average: 0.09, 0.06, 0.01 376959493b5125323c0d3a641f8c3b62c772f13d

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -21,14 +21,13 @@ extern uint8_t data_size;
 #include "cpu/instr/special.h"
 #include "cpu/instr/x87.h"
 
+/* TODO: add more instructions here */
 #include "cpu/instr/cmp.h"
 #include "cpu/instr/push.h"
 #include "cpu/instr/call.h"
 #include "cpu/instr/ret.h"
 #include "cpu/instr/add.h"
 #include "cpu/instr/test.h"
-
-
-/* TODO: add more instructions here */
+#include "cpu/instr/jcc.h"
 
 #endif
---
+++ nemu/include/cpu/instr/jcc.h
@@ -0,0 +1,6 @@
+#ifndef INSTR_JCC_H
+#define INSTR_JCC_H
+
+make_instr_func(jz_short_);
+
+#endif
