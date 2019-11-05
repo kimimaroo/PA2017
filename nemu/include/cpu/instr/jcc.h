3e56622f372be27a94332c6111ec4dd9d2388e42
tracer-pa2017 <tracer@njuics.org>
2019-11-05 13:28:27 +0800

    >  nemu MF1933037 hxj Linux debian9 4.9.0-11-686-pae #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) i686 GNU/Linux  13:28:27 up 1 day, 21:48,  2 users,  load average: 0.00, 0.00, 0.00 3d7d06e5a9ebd177b10a5c776a1968cbe67e5556

--- nemu/include/cpu/instr/jcc.h
+++ nemu/include/cpu/instr/jcc.h
@@ -18,4 +18,21 @@ make_instr_func(jnl_short_);
 make_instr_func(jle_short_);
 make_instr_func(jnle_short_);
 
+make_instr_func(jo_near);
+make_instr_func(jno_near);
+make_instr_func(jb_near);
+make_instr_func(jnb_near);
+make_instr_func(jz_near);
+make_instr_func(jnz_near);
+make_instr_func(jbe_near);
+make_instr_func(jnbe_near);
+make_instr_func(js_near);
+make_instr_func(jns_near);
+make_instr_func(jp_near);
+make_instr_func(jnp_near);
+make_instr_func(jl_near);
+make_instr_func(jnl_near);
+make_instr_func(jle_near);
+make_instr_func(jnle_near);
+
 #endif
