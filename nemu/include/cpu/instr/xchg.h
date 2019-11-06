50a3ba2d3bb4b7972aec1b86d01d4fe69454b5ff
tracer-pa2017 <tracer@njuics.org>
2019-11-06 19:49:15 +0800

    >  nemu MF1933037 hxj Linux debian9 4.9.0-11-686-pae #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) i686 GNU/Linux  19:49:15 up 37 min,  2 users,  load average: 0.04, 0.08, 0.08 d1e02603c8b39a55c5221a2d61ec209eb16f2680

--- nemu/include/cpu/instr/xchg.h
+++ nemu/include/cpu/instr/xchg.h
@@ -3,5 +3,12 @@
 
 make_instr_func(xchg_r2rm_b);
 make_instr_func(xchg_r2rm_v);
+make_instr_func(xchg_ecx);
+make_instr_func(xchg_edx);
+make_instr_func(xchg_ebx);
+make_instr_func(xchg_esp);
+make_instr_func(xchg_ebp);
+make_instr_func(xchg_esi);
+make_instr_func(xchg_edi);
 
 #endif
--- nemu/src/cpu/instr/xchg.c
+++ nemu/src/cpu/instr/xchg.c
@@ -1,5 +1,13 @@
 #include "cpu/instr.h"
 
+#define make_instr_impl_xchg_reg(reg) \
+    make_instr_func(concat3(xchg, _, reg)) {\
+        uint32_t temp = cpu.reg; \
+        cpu.reg = cpu.eax; \
+        cpu.eax = temp; \
+        return 1; \
+    }
+
 static void instr_execute_2op()
 {
     operand_read(&opr_src);
@@ -13,3 +21,10 @@ static void instr_execute_2op()
 
 make_instr_impl_2op(xchg, r, rm, b)
 make_instr_impl_2op(xchg, r, rm, v)
+make_instr_impl_xchg_reg(ecx)
+make_instr_impl_xchg_reg(edx)
+make_instr_impl_xchg_reg(ebx)
+make_instr_impl_xchg_reg(esp)
+make_instr_impl_xchg_reg(ebp)
+make_instr_impl_xchg_reg(esi)
+make_instr_impl_xchg_reg(edi)
